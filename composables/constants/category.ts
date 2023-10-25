b33cd55dd86d71415f3909efda7371a319be682f
diyanavrilina <diyanavrilina@gmail.com>
2023-10-25 07:25:39 +0700

    create project nuxt

---
+++ composables/constants/category.ts
@@ -0,0 +1,18 @@
+export const category: Category[] = [
+    {
+    id: 1,
+    name: "Kacamata",
+    },
+    {
+    id: 2,
+    name: "Dresss",
+    },
+    {
+    id: 3,
+    name: "Sepatu",
+    },
+    ]
+    export interface Category {
+    id? : number,
+    name?: string,
+    }
---
+++ composables/constants/products.ts
@@ -0,0 +1,29 @@
+export const products = [
+  {
+    id: 1,
+    name: "Kacamata casual hitam",
+    category: "Kacamata",
+    price: 15,
+    image: "/images/product1.jpg",
+    isCart: false,
+    description: "Kacamata Fashion dengan desain yang modern dan gaya yang stylish.",
+  },
+  {
+    id: 2,
+    name: "Hyejin Dress",
+    category: "Dress",
+    price: 25,
+    image: "/images/product2.jpg",
+    isCart: false,
+    description: "Dress casual dengan potongan lengan panjang dan desain yang trendi.",
+  },
+  {
+    id: 3,
+    name: "Sepatu sneakers Piero Rusher Lux",
+    category: "Sepatu",
+    price: 35,
+    image: "/images/product3.jpg",
+    isCart: false,
+    description: "Sepatu sneakers dengan gaya streetwear yang trendy.",
+  },
+];
